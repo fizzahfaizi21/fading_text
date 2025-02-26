58deb65cf5faeb1196671eed5f778a3e5c377fb5
fizzahfaizi21 <fizzahfaizi21@gmail.com>
2025-02-26 18:12:07 -0500

    added a dark mode/light mode toggle

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,45 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FadingTextAnimation(),
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: FadingTextAnimation(
+        isDarkMode: _isDarkMode,
+        toggleTheme: toggleTheme,
+      ),
     );
   }
 }
 
 class FadingTextAnimation extends StatefulWidget {
+  final bool isDarkMode;
+  final VoidCallback toggleTheme;
+
+  FadingTextAnimation({required this.isDarkMode, required this.toggleTheme});
+
   @override
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
@@ -31,15 +54,36 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              widget.isDarkMode ? Icons.wb_sunny : Icons.nightlight_round,
+            ),
+            onPressed: widget.toggleTheme,
+            tooltip: widget.isDarkMode
+                ? 'Switch to Light Mode'
+                : 'Switch to Dark Mode',
+          ),
+        ],
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              child: Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              widget.isDarkMode ? 'Dark Mode Enabled' : 'Light Mode Enabled',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
