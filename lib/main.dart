2e57fa44a463e046d2bb545c4ef1f29fc964c745
fizzahfaizi21 <fizzahfaizi21@gmail.com>
2025-02-26 18:45:47 -0500

    added confetti that shows when play is pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -134,14 +135,67 @@ class FadingTextAnimation extends StatefulWidget {
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 
-class _FadingTextAnimationState extends State<FadingTextAnimation> {
+class _FadingTextAnimationState extends State<FadingTextAnimation>
+    with TickerProviderStateMixin {
   bool _isVisible = true;
   Color _textColor = Colors.black;
+  List<ConfettiParticle> _confetti = [];
+  AnimationController? _confettiController;
+  bool _showConfetti = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _confettiController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _confettiController!.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _showConfetti = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _confettiController?.dispose();
+    super.dispose();
+  }
 
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
+      // Trigger confetti
+      _showConfetti = true;
+      _generateConfetti();
+      _confettiController!.forward(from: 0.0);
     });
+  }
+
+  void _generateConfetti() {
+    _confetti = [];
+    final random = Random();
+    for (int i = 0; i < 50; i++) {
+      _confetti.add(
+        ConfettiParticle(
+          x: random.nextDouble() * MediaQuery.of(context).size.width,
+          y: -20.0 - random.nextDouble() * 100,
+          color: Color.fromARGB(
+            255,
+            random.nextInt(255),
+            random.nextInt(255),
+            random.nextInt(255),
+          ),
+          size: 5 + random.nextDouble() * 10,
+          speed: 30 + random.nextDouble() * 50,
+          angle: (random.nextDouble() * 0.2) - 0.1,
+        ),
+      );
+    }
   }
 
   void changeColor(Color color) {
@@ -157,73 +211,98 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       _textColor = Colors.white;
     }
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AnimatedOpacity(
-            opacity: _isVisible ? 1.0 : 0.0,
-            duration: widget.duration,
-            child: Text(
-              widget.title,
-              style: TextStyle(fontSize: 24, color: _textColor),
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            widget.isDarkMode ? 'Dark Mode Enabled' : 'Light Mode Enabled',
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 10),
-          Text(
-            widget.subtitle,
-            style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
-          ),
-          SizedBox(height: 20),
-          Row(
+    return Stack(
+      children: [
+        Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              IconButton(
-                icon: Icon(Icons.color_lens),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Choose Text Color'),
-                        content: SingleChildScrollView(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              colorButton(Colors.red, 'Red'),
-                              colorButton(Colors.blue, 'Blue'),
-                              colorButton(Colors.green, 'Green'),
-                              colorButton(Colors.orange, 'Orange'),
-                              colorButton(Colors.purple, 'Purple'),
-                              colorButton(
-                                  widget.isDarkMode
-                                      ? Colors.white
-                                      : Colors.black,
-                                  'Default'),
-                            ],
-                          ),
-                        ),
+              AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: widget.duration,
+                child: Text(
+                  widget.title,
+                  style: TextStyle(fontSize: 24, color: _textColor),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                widget.isDarkMode ? 'Dark Mode Enabled' : 'Light Mode Enabled',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                widget.subtitle,
+                style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.color_lens),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Choose Text Color'),
+                            content: SingleChildScrollView(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  colorButton(Colors.red, 'Red'),
+                                  colorButton(Colors.blue, 'Blue'),
+                                  colorButton(Colors.green, 'Green'),
+                                  colorButton(Colors.orange, 'Orange'),
+                                  colorButton(Colors.purple, 'Purple'),
+                                  colorButton(
+                                      widget.isDarkMode
+                                          ? Colors.white
+                                          : Colors.black,
+                                      'Default'),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       );
                     },
-                  );
-                },
-                tooltip: 'Change Text Color',
-              ),
-              SizedBox(width: 20),
-              FloatingActionButton(
-                onPressed: toggleVisibility,
-                child: Icon(Icons.play_arrow),
-                tooltip: 'Toggle Visibility',
+                    tooltip: 'Change Text Color',
+                  ),
+                  SizedBox(width: 20),
+                  FloatingActionButton(
+                    onPressed: toggleVisibility,
+                    child: Icon(Icons.play_arrow),
+                    tooltip: 'Toggle Visibility',
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+        if (_showConfetti)
+          AnimatedBuilder(
+            animation: _confettiController!,
+            builder: (context, child) {
+              for (var confetti in _confetti) {
+                confetti.y += confetti.speed * _confettiController!.value;
+                confetti.x += confetti.angle *
+                    confetti.speed *
+                    _confettiController!.value *
+                    10;
+              }
+
+              return CustomPaint(
+                size: Size(
+                  MediaQuery.of(context).size.width,
+                  MediaQuery.of(context).size.height,
+                ),
+                painter: ConfettiPainter(_confetti),
+              );
+            },
+          ),
+      ],
     );
   }
 
@@ -246,4 +325,58 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       ),
     );
   }
+}
+
+class ConfettiParticle {
+  double x;
+  double y;
+  Color color;
+  double size;
+  double speed;
+  double angle; // For sideways movement
+
+  ConfettiParticle({
+    required this.x,
+    required this.y,
+    required this.color,
+    required this.size,
+    required this.speed,
+    required this.angle,
+  });
+}
+
+class ConfettiPainter extends CustomPainter {
+  final List<ConfettiParticle> confetti;
+
+  ConfettiPainter(this.confetti);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var particle in confetti) {
+      if (particle.y < size.height) {
+        final paint = Paint()..color = particle.color;
+
+        // Reduce the downward speed by applying a smaller multiplier
+        double fallSpeed = particle.speed * 0.2; // Slows down falling speed
+        double sideDrift =
+            particle.angle * particle.speed * 0.5; // Less horizontal drift
+
+        particle.y += fallSpeed;
+        particle.x += sideDrift;
+
+        canvas.drawRect(
+          Rect.fromLTWH(
+            particle.x,
+            particle.y,
+            particle.size,
+            particle.size * 1.5,
+          ),
+          paint,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
